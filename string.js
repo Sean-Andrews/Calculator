eac0d86e2d51344d5e531e2d87d8ca6c3efe7679
Sean Andrews <sandrews2332@gmail.com>
2021-07-15 10:46:14 -0400

    Add keyboard press functionality and clean code

--- string.js
+++ string.js
@@ -10,6 +10,7 @@ const unary = document.getElementById('unary-btn');
 const percent = document.getElementById('percent-btn');
 
 // initialize variables and arrays
+// Most are boolean values to check conditions as functions are called
 
 let x;
 let y;
@@ -23,7 +24,8 @@ let isPercent = false;
 let lastEquals = false;
 let total;
 
-// Basic math functions 
+// Create basic math functions 
+// Round to 2 decimal places
 
 function add(x, y) {
     let temp = x + y;
@@ -50,6 +52,7 @@ function divide(x, y) {
 }
 
 // Create operate function
+// Calls basic math function dependent on the sign input
 
 function operate(x, sign, y) {
         sign === "+" ? output.textContent = add(x, y) :
@@ -60,6 +63,8 @@ function operate(x, sign, y) {
 };        
     
 // Function to capture a number and store it in a variable
+// Checks for the presence of a sign...if false, stores a variable as x.
+// All subsequent values are stored as a new y until the user clears the data.
 
 function getNumber(digit) {
     lastEquals = false;
@@ -82,6 +87,7 @@ function getNumber(digit) {
 }
 
 // Function to get the total
+// Checks the presence of a boolean value to see if it is a first equation or a subsequent occasion.
 
 function getTotal() {
     if (!runningTotal) {
@@ -92,9 +98,55 @@ function getTotal() {
     }    
 }
 
-// Function to use keyboard {
+// Function that calls the total when equals is pressed
+// Checks several conditions so as not to call an error when pressed before data is entered
+
+function equalsSign() {
+    if (isSign === false || x === undefined || y === undefined) {;
+        return;
+    }
+    number.length = 0;
+    getTotal();
+    lastEquals = true;
+}    
+
+// Function to use the keyboard 
+// Better way to code this?
+
+function useKeyboard(e) {
+    if (e.keyCode === 48) getNumber(0);  
+    if (e.keyCode === 49) getNumber(1); 
+    if (e.keyCode === 50) getNumber(2); 
+    if (e.keyCode === 51) getNumber(3); 
+    if (e.keyCode === 52) getNumber(4); 
+    if (e.keyCode === 53) getNumber(5); 
+    if (e.keyCode === 54) getNumber(6); 
+    if (e.keyCode === 55) getNumber(7); 
+    if (e.keyCode === 56) getNumber(8); 
+    if (e.keyCode === 57) getNumber(9); 
+    if (e.keyCode === 13) equalsSign();
+    if (e.keyCode === 27) clearAll();
+}
+
+// Function to clear all data
+
+function clearAll() {
+    x = null;
+    y = null;
+    total = null;
+    number.length = 0;
+    sign = "";
+    isSign = false;
+    output.textContent = "0";
+    decimalToggle = false;
+    unaryToggle = false;
+    isPercent = false;
+    runningTotal = false;
+    lastEquals = false;
+}
 
 // Add event listener for numbers
+// Limits the amount of numbers in the display to 9 
 
 [...numButtons].forEach((numBtn) => {
     numBtn.addEventListener('click', () => {
@@ -105,6 +157,7 @@ function getTotal() {
 });   
 
 // Add event listener for decimal button
+// Checks if a boolean value is true...if it is false, adds a decimal
 
 decimal.addEventListener('click', () => {
     if (decimalToggle) return;
@@ -113,6 +166,7 @@ decimal.addEventListener('click', () => {
 });
 
 // Add event listener for unary operator button
+// Toggles the negative sign
 
 unary.addEventListener('click', () => {
     if (number.length === 0) return;
@@ -132,7 +186,9 @@ percent.addEventListener('click', () => {
     getNumber();
 });
 
-// Add event listener for sign
+// Add event listener and functionality for sign.  
+// Uses boolean variables to reset the ability to add decimals or to continue equations before an equals sign is pressed
+// How can I write this as a seperate function to refer to for keyboard functionality?
 
 [...signButtons].forEach((signBtn) => {
     signBtn.addEventListener('click', () => {
@@ -155,32 +211,14 @@ percent.addEventListener('click', () => {
     });
 });
 
-// // Add event listener for equals button
+// Add event listener for equals button
 
-equals.addEventListener('click', () => {
-    if (isSign === false || x === undefined || y === undefined) {;
-        console.log(isSign, x);
-        return;
-    }
-    number.length = 0;
-    console.log(isSign, x);
-    getTotal();
-    lastEquals = true;
-});
+equals.addEventListener('click', equalsSign);
 
-// // // Add event listener for clear button
+// Add event listener for clear button
 
-clear.addEventListener('click', () => {
-    x = null;
-    y = null;
-    total = null;
-    number.length = 0;
-    sign = "";
-    isSign = false;
-    output.textContent = "0";
-    decimalToggle = false;
-    unaryToggle = false;
-    isPercent = false;
-    runningTotal = false;
-    lastEquals = false;
-});
+clear.addEventListener('click', clearAll);
+
+// Add event listener for numeric keyboard presses
+
+window.addEventListener('keydown', useKeyboard);
